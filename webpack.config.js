b2db5c95b209a224d4b6a8eeffa731a9bf0d92fb
Tania Ihuminova <tania.iguminova@corp.kismia.com>
2019-02-23 12:13:12 +0200

    clean config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 // Core
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { resolve } = require('path');
 
 /**
  * object
  * function
  * promise
  */
-module.exports = {
+module.exports = (env) => {
+  console.log('-> env', env);
+  return {
     mode:    'none',
     devtool: false,
+    output: {
+      path: resolve(__dirname, './build'),
+      filename: 'bundle.js'
+    },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './static/template.html',
-            title:    'Учим вебпак! 💪🏼🌟🔫',
-        }),
+      new HtmlWebpackPlugin({
+        template: './static/template.html',
+        title:    'Learn webpack',
+      }),
+      new CleanWebpackPlugin([
+        'dist',
+        'build'
+      ], {
+        root: __dirname,
+        verbose:  true,
+      }),
     ],
+  }
 };
